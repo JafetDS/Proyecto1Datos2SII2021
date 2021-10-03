154b7663b81d50db7f2f341a450a9152e0bf660b
Jafet Dixon <jafet.dixon@gmail.com>
2021-10-03 02:05:28 -0600

    Main
    Main

---
+++ cmake-build-debug/CMakeFiles/Proyecto1Datos2SII2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1Datos2SII2021.dir/main.cpp.obj"
+  "Proyecto1Datos2SII2021.exe"
+  "Proyecto1Datos2SII2021.exe.manifest"
+  "Proyecto1Datos2SII2021.pdb"
+  "libProyecto1Datos2SII2021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1Datos2SII2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
